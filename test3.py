337c31896dc781f7bad5d727d5cb446a81a51222
yeboah326 <asanteg36@gmail.com>
2019-04-16 15:46:23 +0000

    Modifying image recognition

--- test3.py
+++ test3.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #Reading the image as it is
-img = cv2.imread("face1.png")
+img = cv2.imread("Bill.png")
 
 #Reading the image as grayscale image
 gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -15,7 +15,7 @@ faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05,minNeighbors=5)
 for x,y,w,h in faces:
     img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0) , 1)
 
-resized = cv2.resize(img, (1200,800))
+resized = cv2.resize(img, (800,800))
 
 cv2.imshow("Gray", resized)
 
