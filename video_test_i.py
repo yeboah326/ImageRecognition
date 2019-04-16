aaac3b474b496b98f8fd96527c017de37f971a98
yeboah326 <asanteg36@gmail.com>
2019-04-16 17:17:27 +0000

    Capturing real time video in grayscale

--- video_test_i.py
+++ video_test_i.py
@@ -3,16 +3,25 @@ import time
 
 video = cv2.VideoCapture(0)
 
-#check evaluates to True if the camera works and False when otherwise
-check, frame = video.read()
+a = 0
+
+while True:
+    a = a + 1
+    #check evaluates to True if the camera works and False when otherwise
+    check, frame = video.read()
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Video Capturing', gray)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
 
 
-#pauses the program for three seconds
-time.sleep(3)
 
-cv2.imshow('Capturing',frame)
-cv2.waitKey()
-
+print("You have ", a, "frames in your video")
 
 video.release()
 
